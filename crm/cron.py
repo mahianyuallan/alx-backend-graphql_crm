5e9692d26c077b05bc5f5a3bf37a529308c34eda
Allan Mwenja <mwenjaallan18@gmail.com>
2025-11-02 18:49:05 +0300

    Commit

--- crm/cron.py
+++ crm/cron.py
@@ -17,3 +17,31 @@ def log_crm_heartbeat():
 
     with open(log_file, "a") as f:
         f.write(message)
+
+def update_low_stock():
+    from datetime import datetime
+    import requests
+
+    log_file = "/tmp/low_stock_updates_log.txt"
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    mutation = """
+    mutation {
+      updateLowStockProducts {
+        success
+        updatedProducts {
+          name
+          stock
+        }
+      }
+    }
+    """
+
+    response = requests.post("http://localhost:8000/graphql", json={"query": mutation})
+    data = response.json()
+
+    with open(log_file, "a") as f:
+        if data.get("data", {}).get("updateLowStockProducts", {}).get("success"):
+            for product in data["data"]["updateLowStockProducts"]["updatedProducts"]:
+                f.write(f"[{timestamp}] {product['name']} restocked to {product['stock']}\n")
+
